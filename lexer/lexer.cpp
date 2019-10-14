ba5bbc7035c994bf90a24f503aea24dbfcff9655
unknown <liutauras.gaidamavicius@gmail.com>
2019-10-14 10:37:04 +0300

    added unterminated token error and an EOF token

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -10,14 +10,14 @@
 enum TokenType{
     IDENT, LIT_INT, LIT_FLOAT, LIT_STR, OP_PLUS, OP_INCREMENT, OP_MINUS, OP_DECREMENT, OP_L, OP_LE, OP_H, OP_HE, OP_ASSIGN, OP_E,
     OP_PAREN_OPEN, OP_PAREN_CLOSE, OP_CB_OPEN, OP_CB_CLOSE, OP_SB_OPEN, OP_SB_CLOSE, OP_MULT, OP_DIV, OP_LOGIC_AND, OP_LOGIC_OR, OP_LOGIC_NOT, OP_FIELD_ACCESS, OP_COMMA_SEP, OP_SEMICOLON_SEP,
-    KW_IF, KW_ELIF, KW_ELSE, KW_WHILE, KW_FOR, KW_RETURN, KW_BREAK, KW_NEXT, KW_READ, KW_WRITE, KW_CLASS, KW_PRIVATE, KW_PUBLIC, KW_FUNC, KW_INT, KW_FLOAT, KW_BOOL, KW_STRING, KW_VOID, KW_TRUE, KW_FALSE
+    KW_IF, KW_ELIF, KW_ELSE, KW_WHILE, KW_FOR, KW_RETURN, KW_BREAK, KW_NEXT, KW_READ, KW_WRITE, KW_CLASS, KW_PRIVATE, KW_PUBLIC, KW_FUNC, KW_INT, KW_FLOAT, KW_BOOL, KW_STRING, KW_VOID, KW_TRUE, KW_FALSE, EOF_
 };
 
 const std::string TokenNames[] = {"IDENT", "LIT_INT", "LIT_FLOAT", "LIT_STR", "OP_PLUS", "OP_INCREMENT", "OP_MINUS", "OP_DECREMENT", "OP_L", "OP_LE", "OP_H", "OP_HE", "OP_ASSIGN", "OP_E",
                                   "OP_PAREN_OPEN", "OP_PAREN_CLOSE", "OP_CB_OPEN", "OP_CB_CLOSE", "OP_SB_OPEN", "OP_SB_CLOSE", "OP_MULT", "OP_DIV", "OP_LOGIC_AND", "OP_LOGIC_OR", "OP_LOGIC_NOT",
                                   "OP_FIELD_ACCESS", "OP_COMMA_SEP", "OP_SEMICOLON_SEP",
                                   "KW_IF", "KW_ELIF", "KW_ELSE", "KW_WHILE", "KW_FOR", "KW_RETURN", "KW_BREAK", "KW_NEXT", "KW_READ", "KW_WRITE", "KW_CLASS", "KW_PRIVATE", "KW_PUBLIC",
-                                  "KW_FUNC", "KW_INT", "KW_FLOAT", "KW_BOOL", "KW_STRING", "KW_VOID", "KW_TRUE", "KW_FALSE"};
+                                  "KW_FUNC", "KW_INT", "KW_FLOAT", "KW_BOOL", "KW_STRING", "KW_VOID", "KW_TRUE", "KW_FALSE", "EOF"};
 
 const std::map<std::string, TokenType> keywords = {
     {"if", TokenType::KW_IF},
@@ -175,10 +175,19 @@ public:
 
         if(running)
         {
+            
             current_char=' ';
             lex();
-            print_tokens();
+
+            if(current_state != State::S_START){
+                printf("lexer error at %d:%d : unterminated token\n", current_line, current_column);
+                running = false;
+            }else{
+                complete_token(TokenType::EOF_, false, false);
+            }
+
         }
+        if(running) print_tokens();
     }
 
     void lex(){
