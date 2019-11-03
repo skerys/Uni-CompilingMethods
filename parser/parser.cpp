ac92b58a090703d6250afce66d66bb4686e2ee5f
unknown <liutauras.gaidamavicius@gmail.com>
2019-11-03 23:40:23 +0200

    full ast hierarchy

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -3,6 +3,30 @@
 #include <string>
 #include <memory>
 
+enum UnaryOp{
+    POSITIVE, NEGATIVE, NOT
+};
+
+enum ArithOp{
+    ADD, SUB, MUL, DIV
+};
+
+enum CompareOp{
+    E, GE, LE, G, L, NE
+};
+
+enum LogicOp{
+    AND, OR
+};
+
+enum IncDecOp{
+    INC, DEC
+};
+
+enum AccesibilityName{
+    PUBLIC, PRIVATE
+};
+
 class Node{
     virtual void print_node(){
         std::cout << "print node not implemented for type " << typeid(this).name() << std::endl;
@@ -20,7 +44,67 @@ class Stmt : Node{
 
 class Expr : Node{};
 
+class AssignExpr : Expr{
+    std::unique_ptr<Expr> var;
+    std::unique_ptr<Expr> value;
+};
 
+class VarExpr : Expr{
+    std::string name;
+};
+
+class LitExpr : Expr{
+    std::variant<std::string, int, float> lit;
+};
+
+class ParenExpr : Expr{
+    std::unique_ptr<Expr> inside;
+};
+
+class UnaryExpr : Expr{
+    UnaryOp op;
+    std::unique_ptr<Expr> expr;
+};
+
+class ArithExpr : Expr{
+    ArithOp op;
+    std::unique_ptr<Expr> left;
+    std::unique_ptr<Expr> right;
+};
+
+class CompareExpr : Expr{
+    CompareOp op;
+    std::unique_ptr<Expr> left;
+    std::unique_ptr<Expr> right;
+};
+
+class LogicExpr : Expr{
+    LogicOp op;
+    std::unique_ptr<Expr> left;
+    std::unique_ptr<Expr> right;
+};
+
+
+class ArraySubscriptExpr: Expr{
+    std::unique_ptr<Expr> array; //could be a string but how would class.member[1] work then?
+    std::unique_ptr<Expr> index;
+
+};
+
+class FnCallExpr : Expr{
+    std::unique_ptr<Expr> toCall; //could be a string but how would class.method() work then
+    std::vector<std::unique_ptr<Expr>> callParams;
+};
+
+class FieldAccessExpr : Expr{
+    std::unique_ptr<Expr> expr;
+    std::string member;
+};
+
+class IncDecExpr : Expr{
+    IncDecOp op;
+    std::unique_ptr<Expr> expr;
+};
 
 class IfElseStmt : Stmt{
     std::unique_ptr<Expr> condition;
@@ -85,6 +169,20 @@ class FnDecl : Decl{
     std::vector<std::unique_ptr<Param>> params;
     StmtBlock body;
 
+};
+
+class ClassDecl: Decl{
+    std::string name;
+    std::unique_ptr<ClassBody> body;
+};
+
+class ClassBody : Node{
+    std::vector<std::unique_ptr<ClassBodyStmt>> stmts;
+};
+
+class ClassBodyStmt{
+    AccesibilityName access;
+    std::unique_ptr<Decl> decl;
 };
 
 
